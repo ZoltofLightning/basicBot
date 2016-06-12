b2535e20908bd2f182eb4243d02440714a081d5f
ZoltofLightning <zoltoflightning@gmail.com>
2016-06-12 17:36:40 -0600

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -986,7 +986,7 @@
                     return true;
                 }
                 if (basicBot.settings.lockdownEnabled) {
-                    if (perm <== 1) {
+                    if (perm <= 1) {
                         API.moderateDeleteChat(chat.cid);
                         return true;
                     }
